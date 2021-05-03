c8e8d0ab680664466037538dc62920d5b9674f41
desaihd <hetav.desai20@gmail.com>
2021-05-03 05:51:55 +0530

    fis: save room history

--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -8,21 +8,27 @@ export default function History() {
   const { user, db } = useFirebase();
   const { allRooms } = useRoomData();
   const [rooms, setRooms] = useState();
+  const [attendedRoomIds, setAttendedRoomIds] = useState([]);
 
   useEffect(() => {
-    const unsubscribeHistoryRooms = db
-      .collection("users")
+    // const unsubscribeHistoryRooms = db
+    //   .collection("rooms")
+    //   .doc(user?.uid)
+    //   .collection("attendedRooms")
+    //   .onSnapshot((snapshot) => {
+    //     const tempRooms = [];
+    //     snapshot.forEach((doc) => {
+    //       doc.data().id && tempRooms.push(doc.data());
+    //     });
+    //     setRooms(tempRooms);
+    //   });
+    const tempRoomIds = [];
+    db.collection("users")
       .doc(user?.uid)
       .collection("attendedRooms")
       .onSnapshot((snapshot) => {
-        const tempRooms = [];
-        snapshot.forEach((doc) => {
-          doc.data().id &&
-            doc.data().status === "archieved" &&
-            doc.data().visibility === "public" &&
-            tempRooms.push(doc.data());
-        });
-        setRooms(tempRooms);
+        snapshot.forEach((doc) => tempRoomIds.push(doc.data().id));
+        setAttendedRoomIds(tempRoomIds);
       });
     return () => {};
   }, []);
@@ -30,7 +36,18 @@ export default function History() {
   return (
     <div className="container">
       <h1>History</h1>
-      {rooms && rooms.map((room) => <RoomCard room={room} />)}
+      {allRooms &&
+        allRooms.map((room) => {
+          if (
+            room.status === "archieved" &&
+            room.visibility === "public" &&
+            attendedRoomIds.includes(room.id)
+          ) {
+            return <RoomCard room={room} />;
+          } else {
+            return <></>;
+          }
+        })}
     </div>
   );
 }
